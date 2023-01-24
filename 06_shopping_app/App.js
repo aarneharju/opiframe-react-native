29ef2581012a3818d7ad74c5c5930f7fa64637b5
aarneharju <35223549+aarneharju@users.noreply.github.com>
2023-01-24 10:40:40 +0200

    06_shopping_app done.

--- 06_shopping_app/App.js
+++ 06_shopping_app/App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import ShoppingList from './components/ShoppingList';
+import ShoppingForm from './components/ShoppingForm';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+  const [state, setState] = useState({
+    list: [],
+    id: 100
+  })
+
+  const addToList = (item) => {
+    item.id = state.id;
+    setState((state) => {
+      return {
+        list: state.list.concat(item),
+        id: state.id + 1
+      }
+    })
+  }
+
+  const removeItem = (id) => {
+    setState((state) => {
+      let templist = state.list.filter(item => id !== item.id)
+      return {
+        ...state,
+        list: templist
+      }
+    })
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="ShoppingList">
+          {props => <ShoppingList {...props} list={state.list} removeItem={removeItem} />}
+        </Stack.Screen>
+        <Stack.Screen name="Add Item">
+          {props => <ShoppingForm {...props} addToList={addToList} />}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
--- 06_shopping_app/components/ShoppingForm.js
+++ 06_shopping_app/components/ShoppingForm.js
@@ -1,99 +1,107 @@
-import {useState} from 'react';
-import {View,Text,Pressable,TextInput,StyleSheet} from 'react-native';
+import { useState } from 'react';
+import { View, Text, Pressable, TextInput, StyleSheet } from 'react-native';
 
 const ShoppingForm = (props) => {
-	
-	const [state,setState] = useState({
-		type:"",
-		count:"",
-		price:""
+
+	const [state, setState] = useState({
+		type: "",
+		count: "",
+		price: ""
 	})
-	
+
 	const addToList = () => {
 		let item = {
 			...state,
-			id:0
+			id: 0
 		}
 		props.addToList(item);
 		setState({
-			type:"",
-			count:"",
-			price:""
+			type: "",
+			count: "",
+			price: ""
 		})
 	}
-	
-	return(
+
+	return (
 		<View style={styles.container}>
 			<View style={styles.row}>
-				<Text style={[styles.text,styles.label]}>Type:</Text>
-				<TextInput style={[styles.text,styles.input]}
+				<Text style={[styles.text, styles.label]}>Type:</Text>
+				<TextInput style={[styles.text, styles.input]}
 					onChangeText={(text) => {
 						setState((state) => {
-							...state,
-							type:text
+							return {
+								...state,
+								type: text
+							}
 						})
-					}} value={state.type}/>
+					}} value={state.type} />
 			</View>
-				<Text style={[styles.text,styles.label]}>Count:</Text>
-				<TextInput style={[styles.text,styles.input]}
+			<View style={styles.row}>
+				<Text style={[styles.text, styles.label]}>Count:</Text>
+				<TextInput style={[styles.text, styles.input]}
 					onChangeText={(text) => {
 						setState((state) => {
-							...state,
-							count:text
+							return {
+								...state,
+								count: text
+							}
 						})
-					}} value={state.count} inputMode="numeric"/>
+					}} value={state.count} inputMode="numeric" />
 			</View>
-				<Text style={[styles.text,styles.label]}>Price:</Text>
-				<TextInput style={[styles.text,styles.input]}
+			<View style={styles.row}>
+				<Text style={[styles.text, styles.label]}>Price:</Text>
+				<TextInput style={[styles.text, styles.input]}
 					onChangeText={(text) => {
 						setState((state) => {
-							...state,
-							price:text
+							return {
+								...state,
+								price: text
+							}
 						})
-					}} value={state.price} inputMode="numeric"/>
-			</View>
-			<View style={[styles.row,styles.buttonRow]}>
+					}} value={state.price} inputMode="numeric" />
+			</View >
+			<View style={[styles.row, styles.buttonRow]}>
 				<Pressable style={styles.addButton}
 					onPress={addToList}>
 					<Text style={styles.text}>Add</Text>
 				</Pressable>
-			</View>	
-		</View>
+			</View>
+		</View >
 	)
 }
 
 const styles = StyleSheet.create({
-	container:{
-		flex:1,
-		alignItems:"center",
-		justifyContent:"center"
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center"
 	},
-	row:{
-		flex:1,
-		flexDirection:"row",
-		alignItems:"center",
-		justifyContent:"space-between"
+	row: {
+		flex: 1,
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "space-between"
 	},
-	buttonRow:{
-		justifyContent:"center"
+	buttonRow: {
+		justifyContent: "center"
 	},
-	addButton:{
-		height:80,
-		width:110,
-		backgroundColor:"blue",
-		alignItems:"center",
-		justifyContent:"center"
+	addButton: {
+		height: 80,
+		width: 110,
+		backgroundColor: "blue",
+		alignItems: "center",
+		justifyContent: "center"
 	},
-	text:{
-		fontFamily:"sans-serif",
-		fontSize:18
+	text: {
+		fontFamily: "sans-serif",
+		fontSize: 18
 	},
-	label:{
-		fontWeight:"bold"
+	label: {
+		fontWeight: "bold"
 	},
-	input:{
-		width:200,
-		backgroundColor:"lightgreen"
+	input: {
+		width: 200,
+		backgroundColor: "lightgreen"
 	}
 })
 
